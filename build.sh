0561a33361b5e80bde7ab44d9779d927bb2baf84
alvin.eyre <alvin_eyre@acellusacademy.org>
2026-06-01 16:51:49 -0500

    Added whisper.h and improved build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+DIR="$(pwd)"
 git clone https://github.com/ggml-org/whisper.cpp.git
 cd whisper.cpp/bindings/go
 make whisper
+cd $DIR
+rm static-lfs/*
+cp whisper.cpp/include/whisper.h static-lfs/
+cp whisper.cpp/build_go/src/libwhisper.a static-lfs/
+
